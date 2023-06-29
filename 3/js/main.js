c335ef7c562ae8ae94ab36de603afa32923396a2
zzzzDenzzzz <12den24@gmail.com>
2023-06-29 20:48:39 +0300

    3 task completed

---
+++ 3/js/main.js
@@ -0,0 +1,53 @@
+let timer =
+{
+    time: 0,
+    id: null,
+
+    startTimer: function()
+    {
+        this.time = parseInt(document.querySelector("#timer-input").value);
+        
+        if(!isNaN(this.time) && this.time > 0)
+        {
+            console.log("Таймер запущен на "+ this.time + " секунд.");
+            
+            clearInterval(this.id);
+            
+            // ссылка на объект timer
+            let self = this;
+
+            // Добавляем код для вывода времени на странице
+            let timerOutput = document.querySelector("#timer-output");
+        
+            this.id = setInterval(function()
+            {
+                if(self.time == 0)
+                {
+                    console.log("Таймер завершен!");
+                    clearInterval(self.id);
+
+                    // Очищаем поле вывода времени после завершения таймера
+                    timerOutput.innerHTML = "";
+
+                    // Очищаем поле ввода времени после завершения таймера
+                    document.querySelector("#timer-input").value = "";
+                }
+                else
+                {
+                    console.log("Осталось времени:" + self.time + " секунд.");
+                    timerOutput.innerHTML = "Осталось времени: " + self.time + " секунд.";
+                    self.time--;
+                }
+            }, 1000);
+        }
+        else
+        {
+            console.error("Пожалуйста, введите корректное значение времени.");
+        }
+    }
+};
+
+document.querySelector("#start-button").addEventListener("click",function()
+{
+    timer.startTimer();
+});
