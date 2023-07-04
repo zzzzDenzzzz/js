357849f273e913024fec89cb438f692789a4ee35
zzzzDenzzzz <12den24@gmail.com>
2023-07-04 20:30:31 +0300

    all

--- 5/js/albums.js
+++ 5/js/albums.js
@@ -1,56 +1,85 @@
 const service_url = 'https://jsonplaceholder.typicode.com';
 
 $.ajax({
-    url: service_url + '/albums',        
-    method: 'get',            
-    dataType: 'json',       
-    success: function(data){ 
-	    data.forEach(album => {
-            drawAlbum(album)
-        });
+  url: service_url + '/albums',
+  method: 'get',
+  dataType: 'json',
+  success: function (data) {
+    data.forEach(album => {
+      drawAlbum(album)
+    });
 
-        loadPhotos();
-    }
+    loadPhotos();
+
+  }
 });
 
-function drawAlbum(album){
-    const div = document.createElement('div');
-    div.classList.add('album');
+function drawAlbum(album) {
+  const div = document.createElement('div');
+  div.classList.add('album');
 
-    const title = document.createElement('h3')
-    title.innerText = album.title;
+  const title = document.createElement('h3')
+  title.innerText = album.title;
 
-    const imgBox = document.createElement('div');
-    imgBox.setAttribute('id', 'commentsBox_'+ album.id)
-    imgBox.classList.add('img-box')
+  const photosBox = document.createElement('div');
+  photosBox.setAttribute('id', 'photosBox_' + album.id)
+  photosBox.classList.add('photos-box')
 
-    div.append(title);
-    div.append(imgBox)
+  const button = document.createElement('span');
+  button.innerText = 'Photo';
+  button.classList.add('photos_button')
 
-    document.getElementById('albums').append(div);
+  div.append(title);
+  div.append(button);
+  div.append(photosBox);
+
+  addOpenButtonListener(button);
+
+  document.getElementById('albums').append(div);
 }
 
-function loadPhotos(){
-    $.ajax({
-        url: service_url + '/photos',        
-        method: 'get',            
-        dataType: 'json',       
-        success: function(data){ 
-            data.forEach(photo => {
-                drawPhoto(photo)
-            });
-        }
-    });
+function addOpenButtonListener(button) {
+
+  button.addEventListener('click', function (e) {
+    let photoBox = e.target.parentElement.querySelector('.photos-box');
+
+    if (photoBox.style.display === 'block'){
+      photoBox.style.display = 'none'
+    } else {
+      photoBox.style.display = 'block';
+    }
+  })
 }
 
-function drawPhoto(photo){
-    const div = document.createElement('div');
-    div.classList.add('photo');
+function loadPhotos() {
+  $.ajax({
+    url: service_url + '/photos',
+    method: 'get',
+    dataType: 'json',
+    success: function (data) {
+      data.forEach(photo => {
 
-    const img = document.createElement('img')
-    img.setAttribute('src', photo.url);
+        drawFoto(photo)
 
-    div.append(img);
+      });
+    }
+  });
+}
 
-    document.getElementById('albums').append(div);
+function drawFoto(photo) {
+  const div = document.createElement('div');
+  div.classList.add('photo');
+
+  const title = document.createElement('h3')
+  title.innerText = photo.title;
+
+  const urlPhoto = document.createElement('img');
+  urlPhoto.setAttribute('src', photo.thumbnailUrl);
+
+  div.append(title);
+  div.append(urlPhoto);
+
+  const photosBox = document.getElementById('photosBox_' + photo.albumId);
+
+  photosBox.append(div);
 }
